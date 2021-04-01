169172c73ae5b55d521a3ba1ba0f488534a64d5b
Jordy <j.naus@student.ru.nl>
2021-04-01 11:00:11 +0200

    Function for FLAC to WAV conversion

---
+++ src/util.py
@@ -0,0 +1,12 @@
+import numpy as np
+import scipy as scipy
+import os
+import sys
+from scipy.io import wavfile
+import soundfile as sf
+
+
+def rewrite_flacs_as_wavs(path):
+
+    
+    print("done")
