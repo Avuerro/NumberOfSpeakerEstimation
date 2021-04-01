9b10e72e9946c82b6c70c60d0fbd84714fb6ce9e
Jordy <j.naus@student.ru.nl>
2021-04-01 11:01:03 +0200

    Function for FLAC to WAV conversion

--- src/util.py
+++ src/util.py
@@ -6,7 +6,20 @@ from scipy.io import wavfile
 import soundfile as sf
 
 
-def rewrite_flacs_as_wavs(path):
+def flacs_to_wavs(new_dir = "./data/wavs/"):
+    for subdir, dirs, files in os.walk("./data/LibriSpeech/"):
 
-    
-    print("done")
+        for file in files:
+            #print( os.path.join(subdir, file))
+            filepath = subdir + os.sep + file
+
+            if filepath.endswith(".flac"):
+                data, samplerate = sf.read(filepath)
+
+                filepath = filepath.replace(file, "")
+                clean_filepath = filepath.replace("./data/LibriSpeech/dev-clean/", "")
+                new_file = file.replace('.flac', '.wav')
+                if not os.path.exists(new_dir+clean_filepath):
+                    os.makedirs(new_dir+filepath)
+
+                wavfile.write(new_dir+clean_filepath+new_file, samplerate, data)
