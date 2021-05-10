c5a2d787199a6079c5dc53af0ca2fcdd068f46cb
Jordy <j.naus@student.ru.nl>
2021-05-10 14:16:05 +0200

    Flipped STFT for proper representation

--- src/util.py
+++ src/util.py
@@ -225,6 +225,7 @@ def create_spectrograms(input_dir, spectro_output_dir, n_fft=400,hop_length=160,
 
                 new_file_name = os.path.join(output_dir,file_name)
                 sample,sr = sf.read(audio_file_location)
-                X_stft = stft(sample, n_fft=400,hop_length=160,window=signal.windows.hann).T
+                X_stft = np.flipud(stft(sample, n_fft=400,hop_length=160,window=signal.windows.hann).T)
+                
 
                 plt.imsave(new_file_name, X_stft, cmap = 'hot')
