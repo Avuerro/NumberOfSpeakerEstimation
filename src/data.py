53735c72fe554be10db1668e6d49f4fd5bfa1eb5
Jordy <j.naus@student.ru.nl>
2021-05-12 09:00:03 +0200

    val split and rescaling

--- src/data.py
+++ src/data.py
@@ -3,10 +3,11 @@ import keras
 from keras import backend as k 
 
 
-def create_tensorflow_dataset(data_dir, batch_size, data_format='channels_first',color_mode='grayscale', target_size=(500,201) ,datagenerator=None):
+def create_tensorflow_dataset(data_dir, batch_size, data_format='channels_first',color_mode='grayscale', target_size=(500,201) ,datagenerator=None, val_split = None):
     if datagenerator is None:
-        datagenerator = tf.keras.preprocessing.image.ImageDataGenerator()
+        datagenerator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255,validation_split = val_split)
     
 
-    dataset =   tf.keras.preprocessing.image.DirectoryIterator(data_dir, datagenerator,batch_size=batch_size, data_format=data_format, color_mode=color_mode, target_size=target_size)
-    return dataset
+    train_dataset =   tf.keras.preprocessing.image.DirectoryIterator(data_dir, datagenerator,batch_size=batch_size, data_format=data_format, color_mode=color_mode, target_size=target_size, subset = 'training')
+    validation_dataset =   tf.keras.preprocessing.image.DirectoryIterator(data_dir, datagenerator,batch_size=batch_size, data_format=data_format, color_mode=color_mode, target_size=target_size, subset = 'validation')
+    return train_dataset, validation_dataset
