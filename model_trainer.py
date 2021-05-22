26d629c7f9d5d547d3b08208341dcc548f25a6ea
Chihab <chihabamghane@gmail.com>
2021-05-22 13:18:52 +0200

    modified run parameters

--- model_trainer.py
+++ model_trainer.py
@@ -7,14 +7,14 @@ from wandb.keras import WandbCallback
 
 import datetime
 # Parameters #
-DATA_DIR = '../'
+DATA_DIR = '/vol/tensusers3/camghane/ASR/LibriSpeech/train-data/train-clean-100/merged/train/*/*.wav'
 BATCH_SIZE = 32
-SAVE_MODEL_DIR='./weights'
+SAVE_MODEL_DIR = '/vol/tensusers3/camghane/ASR/weights'
 ## Model ##
 LEARNING_RATE = 0.001
 ### Training ###
 EPOCHS = 10
-VALIDATION_SPLIT = 0.3
+VALIDATION_SPLIT = 0.2
 
 # callbacks TODO Move to separate file ?
 ## model callbacks
@@ -38,10 +38,10 @@ model_learning_rate_callback = tf.keras.callbacks.ReduceLROnPlateau(
 log_dir = "./logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 ## init wandb project
-wandb.init(project='asr_speaker_estimation', entity='chickens4peace',config=tf.compat.v1.flags.FLAGS, sync_tensorboard=True)
+wandb.init(project='asr_speaker_estimation', entity='chickens4peace',config=tf.compat.v1.flags.FLAGS, sync_tensorboard=True, dir='/vol/tensusers3/camghane/ASR/run_metadata')
 
 # init dataset
-training_data, validation_data = data.create_tensorflow_dataset(DATA_DIR, batch_size=32, val_split = VALIDATION_SPLIT)
+training_data, validation_data = data.DataSet(DATA_DIR, val_split=VALIDATION_SPLIT)
 ## init model
 model = crnn.CRNN(LEARNING_RATE).get_model()
 ### start training
